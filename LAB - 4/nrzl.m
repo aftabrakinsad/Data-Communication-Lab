53f0f86dd79bd12346379070a66dcfe786d43dce
Rakin Sad Aftab <20-41991-1@student.aiub.edu>
2022-06-28 10:19:55 +0600

    Add files via upload

---
+++ LAB - 4/nrzl.m
@@ -0,0 +1,27 @@
+clc
+bit_stream = [0 0 1 0 0 1 0 0 0 1 1 0];
+no_bits = length(bit_stream);
+bit_rate = 4000; % 4 kbps
+pulse_per_bit = 1; % for polar nrz-L
+pulse_duration = 1/((pulse_per_bit)*(bit_rate));
+no_pulses = no_bits*pulse_per_bit;
+samples_per_pulse = 500;
+fs = (samples_per_pulse)/(pulse_duration);
+t = 0:1/fs:(no_pulses)*(pulse_duration);
+no_samples = length(t); % total number of samples
+dig_sig = zeros(1, no_samples);
+max_voltage = 5;
+min_voltage = 0;
+for i = 1:no_bits
+if bit_stream(i) == 0
+dig_sig(((i-1) *(samples_per_pulse)+1):i*(samples_per_pulse)) = max_voltage*ones (1, samples_per_pulse);
+else
+dig_sig(((i-1) *(samples_per_pulse)+1):i*(samples_per_pulse)) = min_voltage*ones(1,samples_per_pulse);
+end 
+end
+plot(t,dig_sig,'linewidth',1.5)
+grid on
+xlabel('time in seconds')
+ylabel('Voltage')
+ylim([(min_voltage - (max_voltage)*0.2) (max_voltage+max_voltage*0.2)])
+title(['Polar NRZ-L for ',num2str(bit_stream),' ID:20-43246-1 SL:36'])
